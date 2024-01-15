6237f950f317440adcd14728648568ff7ea0b6ac
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2024-01-15 21:34:28 +0700

    get refresh token API

--- routes/refreshTokens.js
+++ routes/refreshTokens.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const refreshTokenHandler = require('./handler/refresh-tokens');
 
+router.get('/', refreshTokenHandler.getToken);
 router.post('/', refreshTokenHandler.create);
 
 module.exports = router;
